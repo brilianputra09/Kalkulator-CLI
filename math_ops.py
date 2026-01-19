85d05881ad775654ebe5901d1ccee7c398bc0aae
brilianputra09 <brilianputra09@gmail.com>
2026-01-19 15:50:45 +0700

    Create math_ops.py
    Tambah operasi matematika dasar

---
+++ math_ops.py
@@ -0,0 +1,13 @@
+def tambah(a, b):
+    return a + b
+
+def kurang(a, b):
+    return a - b
+
+def kali(a, b):
+    return a * b
+
+def bagi(a, b):
+    if b == 0:
+        return "Error: pembagian dengan nol"
+    return a / b
